3512755e43c439f6a44408dafc4858a40c738f83
Torralz <pmario.ceatorralba@gmail.com>
2026-04-05 15:41:30 +0200

    reformateo de las carpetas y borrada carpeta de copia

---
+++ unimano/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file LICENSE.rst or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "/home/mario/esp/esp-idf/components/bootloader/subproject")
+  file(MAKE_DIRECTORY "/home/mario/esp/esp-idf/components/bootloader/subproject")
+endif()
+file(MAKE_DIRECTORY
+  "/home/mario/TFG/unimano/build/bootloader"
+  "/home/mario/TFG/unimano/build/bootloader-prefix"
+  "/home/mario/TFG/unimano/build/bootloader-prefix/tmp"
+  "/home/mario/TFG/unimano/build/bootloader-prefix/src/bootloader-stamp"
+  "/home/mario/TFG/unimano/build/bootloader-prefix/src"
+  "/home/mario/TFG/unimano/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/home/mario/TFG/unimano/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/home/mario/TFG/unimano/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
